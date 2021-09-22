28ceace9092e581a3a3f78673f9b2ebc96685f7a
dahkimr <34751044+dahkimr@users.noreply.github.com>
2021-09-22 15:33:06 -0400

    Updates

---
+++ precache-manifest.28c33bc220d0491fa3097db6a3d7db2b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "212938da7258424e0c7c1503e4e1ba7c",
+    "url": "/cookies-for-boredom/index.html"
+  },
+  {
+    "revision": "7b6bb12ad53ede6a1e09",
+    "url": "/cookies-for-boredom/static/css/main.5221f9a3.chunk.css"
+  },
+  {
+    "revision": "ad1586b01ac832de57cc",
+    "url": "/cookies-for-boredom/static/js/2.94e69927.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/cookies-for-boredom/static/js/2.94e69927.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b6bb12ad53ede6a1e09",
+    "url": "/cookies-for-boredom/static/js/main.5142823e.chunk.js"
+  },
+  {
+    "revision": "40a92668b2f007d69e5e",
+    "url": "/cookies-for-boredom/static/js/runtime-main.af0aa072.js"
+  },
+  {
+    "revision": "afe3625b9e897e9254df4d5e042a276c",
+    "url": "/cookies-for-boredom/static/media/cookie.afe3625b.png"
+  },
+  {
+    "revision": "47fa68e720d370bed7efc2356fa36a98",
+    "url": "/cookies-for-boredom/static/media/cookie_jar.47fa68e7.png"
+  }
+]);
